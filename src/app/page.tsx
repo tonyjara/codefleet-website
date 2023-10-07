2d66b3c923b3fbd036c3374773a5f9ddb1aa9771
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-10-07 17:43:34 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ export default function Home() {
   return (
     <main className="">
       <div>
+
+        
         <h1>Hello Contributors!</h1>
 
 
 
-        
+
         <h2>This is to test prettier</h2>
       </div>
     </main>
